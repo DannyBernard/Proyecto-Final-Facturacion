94d6942230fa03a43e9d5e193833fc7097c0331c
Danny Bernard <Danny Bernard@DESKTOP-ITSUOPV>
2018-11-02 00:20:47 -0400

    Entidad Cliente Listo

---
+++ Proyecto final de Facturacion/Entidades/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final_de_Facturacion.Entidades
+{
+  public  class Cliente
+    {
+        [Key]
+        public int IDCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Cliente()
+        {
+            IDCliente = 0;
+            Nombre = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+
+        }
+
+    }
+}
