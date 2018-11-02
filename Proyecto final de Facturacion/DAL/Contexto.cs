097231dc4db6198d3b070ec910d80cc25018a4dd
Danny Bernard <Danny Bernard@DESKTOP-ITSUOPV>
2018-11-02 00:27:21 -0400

    Contexto en proceso

---
+++ Proyecto final de Facturacion/DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Proyecto_final_de_Facturacion.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final_de_Facturacion.DAL
+{
+   public class Contexto: DbContext
+    {
+        public DbSet<Usuario> Usario { get; set; }
+        public DbSet<Articulos> Articulos{ get; set; }
+        public DbSet<Cliente> Clientes{ get; set; }
+        public DbSet<Credito> Creditos { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
